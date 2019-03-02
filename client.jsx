9ff4eff89f176de52f8191e6ebfc51d97bb8d84d
JosephJvB <joevanbo@pm.me>
2019-03-02 23:34:19 +1300

    hooking it up!!

--- client.jsx
+++ client.jsx
@@ -1,46 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { render } from 'react-dom'
+
+import styles from './styles'
 
 document.addEventListener('DOMContentLoaded', () => {
   render (<App />, document.getElementById('mount'))
 })
 
-const container = {
-  margin: '5rem auto',
-  maxWidth: '400px',
-  display: 'flex',
-  flexDirection: 'column'
-}
-const title = {marginBottom: '2px', textAlign: 'center'}
-const subtitle = {marginBottom: '1rem', textAlign: 'center'}
-const button = {width: '-moz-max-Content', maxWidth: 'fit-content', padding: '0.5rem 1.2rem', fontWeight: '500', fontSize: '16px', margin: '2rem auto', borderRadius: '5px', backgroundColor: 'rgb(120, 217, 110)'}
-const input = {margin: '0 0 1rem 1rem', textAlign: 'center', height: '25px', fontSize: '16px', backgroundColor: '#f3f3bf', borderRadius: '100px'}
-const label = {margin: '0', fontSize: '28px'}
-const labels = {display: 'flex', flexDirection: 'column', margin: '0 10px 0 0', maxWidth: '80px'}
-const inputs = {display: 'flex', flexDirection: 'column', margin: '0', width: '100%'}
-const form = {display: 'flex', flexDirection: 'row', margin: '1rem'}
-const footer = {position: 'absolute', bottom: '20px', textAlign: 'center', width: '100%', color: 'grey'}
-
 const App = () => {
+
+  // hooks are dooope
+  const [token, setToken] = useState('')
+  const [formData, setFormData] = useState({})
+
+  const send = async () => {
+    const {email, password} = formData
+    const headers = {'Content-Type': 'application/json'}
+    const body = JSON.stringify({email, not_a_password: password})
+    const res = await fetch('/api/auth/register', {method: 'POST', headers, body})
+    const token = await res.text()
+    setToken(token)
+  }
+
+  const handleChange = e => {
+    e.persist() // react thing: synthetic events
+    setFormData({
+      ...formData,
+      [e.target.type]: e.target.value
+    })
+  }
+
   return (
     <div>
-      <div style={{...container}}>
-        <h1 style={{...title}}>Welcome!</h1>
-        <p style={{...subtitle}}><i>So glad you made it</i></p>
-        <div style={{...form}}>
-          <div style={{...labels}}>
-            <label style={{...label}}>📧</label>
-            <label style={{...label}}>🔑</label>
+      <div style={{...styles.container}}>
+        <h1 style={{...styles.title}}>Welcome!</h1>
+        <p style={{...styles.subtitle}}><i>So glad you made it</i></p>
+        <div style={{...styles.form}}>
+          <div style={{...styles.labels}}>
+            <label style={{...styles.label}}>📧</label>
+            <label style={{...styles.label}}>🔑</label>
           </div>
-          <div style={{...inputs}}>
-            <input style={{...input}} type="email"/>
-            <input style={{...input}} type="password"/>
+          <div style={{...styles.inputs}}>
+            <input onChange={handleChange} style={{...styles.input}} type="email"/>
+            <input onChange={handleChange} style={{...styles.input}} type="password"/>
           </div>
         </div>
-        <button style={{...button}}>SUBMIT</button>
+        <button onClick={send} style={{...styles.button}}>SUBMIT</button>
       </div>
-      <footer style={{...footer}}>
-        <p>footer text footer text footer text footer text footer text footer text</p>
+      <footer style={{...styles.footer}}>
+        {token ? <p>token: {token.substring(0, 13)}...</p> : <p>footer text footer text footer text footer text footer text footer text</p>}
         <p>all rights are dope yaaaah yeet</p>
       </footer>
     </div>
---
+++ styles.js
@@ -0,0 +1,24 @@
+// lord forgive me for i have sinned
+const container = {margin: '5rem auto', maxWidth: '400px', display: 'flex', flexDirection: 'column'}
+const title = {marginBottom: '2px', textAlign: 'center'}
+const subtitle = {marginBottom: '1rem', textAlign: 'center'}
+const button = {width: '-moz-max-Content', maxWidth: 'fit-content', padding: '0.5rem 1.2rem', fontWeight: '500', fontSize: '16px', margin: '2rem auto', borderRadius: '5px', backgroundColor: 'rgb(120, 217, 110)'}
+const input = {margin: '0 0 1rem 1rem', textAlign: 'center', height: '25px', fontSize: '16px', backgroundColor: '#f3f3bf', borderRadius: '100px'}
+const label = {margin: '0', fontSize: '28px'}
+const labels = {display: 'flex', flexDirection: 'column', margin: '0 10px 0 0', maxWidth: '80px'}
+const inputs = {display: 'flex', flexDirection: 'column', margin: '0', width: '100%'}
+const form = {display: 'flex', flexDirection: 'row', margin: '1rem'}
+const footer = {position: 'absolute', bottom: '20px', textAlign: 'center', width: '100%', color: 'grey'}
+
+module.exports = {
+  container,
+  title,
+  subtitle,
+  button,
+  input,
+  label,
+  labels,
+  inputs,
+  form,
+  footer 
+}
