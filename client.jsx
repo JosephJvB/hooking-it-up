eaab3982aa6c60b55751d88cc91639198f16bdf3
JosephJvB <joevanbo@pm.me>
2019-03-13 12:09:23 +1300

    svg fades in and out, form inputs are controlled: hook-state single source of truth

--- client.jsx
+++ client.jsx
@@ -26,33 +26,36 @@ const Home = () => {
 
   // hooks are dooope
   const [token, setToken] = useState('')
-  const [formData, setFormData] = useState({})
+  const [formData, setFormData] = useState({email: '', not_a_password: ''})
   const [coords, setCoords] = useState({})
   const hasCoords = Object.keys(coords).length > 0
 
-  useEffect(() => console.log('Only call me on mount please :)'), [])
+  // useEffect(() => console.log('Only call me on mount please :)'), [])
   // useEffect(() => console.log('SOMETHING is changing!')) // any re-render (componentDidUpdate)
   // watch a particular piece of component state
-  useEffect(() => console.log('token is changing!'), [token])
-  useEffect(() => console.log('formData is changing!'), [formData])
+  // useEffect(() => console.log('token is changing!'), [token])
+  // useEffect(() => console.log('formData is changing!'), [formData])
   
   const send = async (e) => {
     e.persist()
-    const {text: email, password} = formData
-    if(!email || !password) {
+    const {email, not_a_password} = formData
+    if(!email || !not_a_password) {
       console.error('dont test me I\'ll BOP ya')
       return
     }
     const headers = {'Content-Type': 'application/json'}
-    const body = JSON.stringify({email, not_a_password: password})
+    const body = JSON.stringify(formData)
     try {
       const res = await fetch('/api/auth/register', {method: 'POST', headers, body})
       const token = await res.text()
-      click({ // send the svg pointer to the token element
-        target: document.getElementById('token'),
-        persist: e.persist
-      })
       setToken(token)
+      setFormData({email: '', not_a_password: ''})
+      setTimeout(() => {
+        showPointer({ // send svg pointer to the token element
+          target: document.getElementById('token'),
+          persist: e.persist
+        })
+      }, 1000)
     } catch (e) {
       console.error(e)
     }
@@ -60,25 +63,40 @@ const Home = () => {
 
   const handleChange = e => {
     e.persist() // react thing: synthetic events
+    const dataType = e.target.getAttribute('data-type')
+    // set formData keys as inputElements data-type attribute
     setFormData({
       ...formData,
-      [e.target.type]: e.target.value
+      [dataType]: e.target.value
     })
   }
 
-  const click = e => {
+  const showPointer = e => {
     e.persist()
     const { bottom, right, height } = e.target.getBoundingClientRect()
+    const svgEl = document.getElementById('svg-box')
+    // if exists, fade out
+    if(svgEl) {
+      svgEl.classList.remove('fade-in')
+      svgEl.classList.add('fade-out')
+    }
     // setCoords({ // for circle
     //   x: right + height,
     //   y: bottom - height / 2,
     //   r: 5
     // })
-    setCoords({
-      p1: `${right + 10},${bottom - height / 2}`,
-      p2: `${right + 28},${bottom - height / 2 - 8}`,
-      p3: `${right + 28},${bottom - height / 2 + 8}`
-    })
+    setTimeout(() => {
+      // if exists, remove fadeout class & fade in
+      if(svgEl) {
+        svgEl.classList.remove('fade-out')
+        svgEl.classList.add('fade-in')
+      }
+      setCoords({
+        p1: `${right + 10},${bottom - height / 2}`,
+        p2: `${right + 28},${bottom - height / 2 - 8}`,
+        p3: `${right + 28},${bottom - height / 2 + 8}`
+      })
+    }, 300)
   }
 
   const points = hasCoords && coords.p1 + ' ' + coords.p2 + ' ' + coords.p3
@@ -86,23 +104,25 @@ const Home = () => {
   return (
     <div>
       <div className="container">
-        <h1 onClick={click} className="title">Welcome!</h1>
-        <p onClick={click} className="subtitle"><i>So glad you made it</i></p>
+        <h1 onClick={showPointer} className="title">Welcome!</h1>
+        <p onClick={showPointer} className="subtitle"><i>So glad you made it</i></p>
         <div className="form">
           <div className="emoji-labels">
             <label className="label">📧</label>
             <label className="label">🔑</label>
           </div>
           <div className="input-container">
-            <svg style={{position:'absolute',left:0, top: 0, height: '100vh', width: '100vw', zIndex: -1}}>
-              {/* {hasCoords && <circle cx={coords.x} cy={coords.y} r={coords.r} stroke="red" strokeWidth="2" fill="none" />} */}
-              {hasCoords && <polygon points={points} stroke="#f45f42" strokeWidth="2" fill="#f45f42" />}
-            </svg>
-            <input onClick={click} onChange={handleChange} className="input" type="text"/>
-            <input onClick={click} onChange={handleChange} className="input" type="password"/>
+            {hasCoords && <div id="svg-box" className="svg-box">
+              <svg style={{position:'absolute',left:0, top: 0, height: '100vh', width: '100vw', zIndex: -1}}>
+                {/* {hasCoords && <circle cx={coords.x} cy={coords.y} r={coords.r} stroke="red" strokeWidth="2" fill="none" />} */}
+                <polygon points={points} stroke="#f45f42" strokeWidth="2" fill="#f45f42" />
+              </svg>
+            </div>}
+            <input data-type="email" onClick={showPointer} value={formData.email} onChange={handleChange} className="input" type="text"/>
+            <input data-type="not_a_password" onClick={showPointer} value={formData.not_a_password} onChange={handleChange} className="input" type="password"/>
           </div>
         </div>
-        <button onClick={(e) => {click(e); send(e)}} className="submit-button">SUBMIT</button>
+        <button onClick={(e) => {showPointer(e); send(e)}} className="submit-button">SUBMIT</button>
       </div>
       <footer className="footer">
         <p id="token">
