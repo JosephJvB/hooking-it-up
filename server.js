5ebfd6f6ab882f1e15b4dc385d9e52f093e32f01
JosephJvB <joevanbo@pm.me>
2019-03-13 15:40:21 +1300

    add https server to server file

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ server.use(helmet())
 server.use(express.static(path.join(__dirname, 'dist'), {/*dotfiles: 'allow'*/})) // couldnt get dotfiles through no matter what!
 server.use(express.json())
 
-server.use('/.well-known/acme-challenge', require('./ssl')) // load ssl router after loading vars onto process.env
+if(process.env.NODE_ENV === 'production') {
+  // open https server and register route to respond to ssl challenge
+  initSecureServer(server)
+  server.use('/.well-known/acme-challenge', require('./ssl')) // load ssl router after loading vars onto process.env
+}
 server.use('/api/auth', AuthRouter)
 server.get('*', (req, res, next) => res.sendFile(path.join(__dirname, 'dist/index.html')))
 
--- util.js
+++ util.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const https = require('https')
 
 const CONF_PATH = path.join(__dirname, 'SECRETS.json')
 
@@ -17,6 +18,23 @@ const loadEnv = () => {
   }
 }
 
+const initSecureServer = server => {
+  const creds = {
+    key: fs.readFileSync('/etc/letsencrypt/live/joevanbo.icu/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/joevanbo.icu/cert.pem'),
+    ca: fs.readFileSync('/etc/letsencrypt/live/joevanbo.icu/chain.pem'),
+  }
+  https.createServer(creds, server).listen(443, (err) => {
+    // dunno if this is legit
+    if(err) {
+      console.log('error, exiting', err)
+      process.exit(1)
+    }
+    console.log('HTTPS server up on 443')
+  })
+}
+
 module.exports = {
-  loadEnv
+  loadEnv,
+  initSecureServer
 }
